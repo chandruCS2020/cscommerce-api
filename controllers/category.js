42b14495a18d5f9c2a61b0e697284ee4ea51ce8f
Chandru <chandru6501@gmail.com>
2022-05-24 08:06:05 +0530

    test

--- controllers/category.js
+++ controllers/category.js
@@ -33,16 +33,17 @@ exports.update = (req, res) => {
 // console.log('req.body', req.body);
 // console.log('category update param', req.params.categoryId);
 // const category = req.category;
-Category.name = req.body.name;
-console.log(req.body.name);
-Category.save((err, data) => {
-    if (err) {
-    return res.status(400).json({
-        error: errorHandler(err),
-    });
-    }
-    res.json(data);
-});
+// Category.name = req.body.name;
+// console.log(req.body.name);
+// Category.save((err, data) => {
+//     if (err) {
+//     return res.status(400).json({
+//         error: errorHandler(err),
+//     });
+//     }
+//     res.json(data);
+// });
+res.json(req.body)
 };
 
 exports.remove = (req, res) => {
